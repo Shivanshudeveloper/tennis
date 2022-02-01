ff396c8e6c6eae726aaf3eea38e8a77c7a046ac1
hariom-ojha23 <hari_om.scsebtech@galgotiasuniversity.edu.in>
2022-02-01 17:59:33 +0530

    creating upload buttons and three fields

--- client2/src/components/account/account-profile-details.js
+++ client2/src/components/account/account-profile-details.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from "react";
 import {
   Box,
   Button,
@@ -7,63 +7,55 @@ import {
   CardHeader,
   Divider,
   Grid,
-  TextField
-} from '@mui/material';
+  TextField,
+} from "@mui/material";
 
 const states = [
   {
-    value: 'alabama',
-    label: 'Alabama'
+    value: "alabama",
+    label: "Alabama",
   },
   {
-    value: 'new-york',
-    label: 'New York'
+    value: "new-york",
+    label: "New York",
   },
   {
-    value: 'san-francisco',
-    label: 'San Francisco'
-  }
+    value: "san-francisco",
+    label: "San Francisco",
+  },
 ];
 
 export const AccountProfileDetails = (props) => {
+  const logoInput = useRef();
+
   const [values, setValues] = useState({
-    firstName: 'Katarina',
-    lastName: 'Smith',
-    email: 'demo@devias.io',
-    phone: '',
-    state: 'Alabama',
-    country: 'USA'
+    firstName: "Katarina",
+    lastName: "Smith",
+    email: "demo@devias.io",
+    phone: "",
+    state: "Alabama",
+    country: "USA",
+    tennisCourtName: "",
+    tennisCourtDesc: "",
+    tennisCourtTag: "",
+    tennisCourtLogo: "",
   });
 
   const handleChange = (event) => {
     setValues({
       ...values,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   };
 
   return (
-    <form
-      autoComplete="off"
-      noValidate
-      {...props}
-    >
+    <form autoComplete="off" noValidate {...props}>
       <Card>
-        <CardHeader
-          subheader="The information can be edited"
-          title="Profile"
-        />
+        <CardHeader subheader="The information can be edited" title="Profile" />
         <Divider />
         <CardContent>
-          <Grid
-            container
-            spacing={3}
-          >
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
+          <Grid container spacing={3}>
+            <Grid item md={6} xs={12}>
               <TextField
                 fullWidth
                 helperText="Please specify the first name"
@@ -75,11 +67,7 @@ export const AccountProfileDetails = (props) => {
                 variant="outlined"
               />
             </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
+            <Grid item md={6} xs={12}>
               <TextField
                 fullWidth
                 label="Last name"
@@ -90,11 +78,7 @@ export const AccountProfileDetails = (props) => {
                 variant="outlined"
               />
             </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
+            <Grid item md={6} xs={12}>
               <TextField
                 fullWidth
                 label="Email Address"
@@ -105,11 +89,7 @@ export const AccountProfileDetails = (props) => {
                 variant="outlined"
               />
             </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
+            <Grid item md={6} xs={12}>
               <TextField
                 fullWidth
                 label="Phone Number"
@@ -120,11 +100,7 @@ export const AccountProfileDetails = (props) => {
                 variant="outlined"
               />
             </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
+            <Grid item md={6} xs={12}>
               <TextField
                 fullWidth
                 label="Country"
@@ -135,11 +111,7 @@ export const AccountProfileDetails = (props) => {
                 variant="outlined"
               />
             </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
+            <Grid item md={6} xs={12}>
               <TextField
                 fullWidth
                 label="Select State"
@@ -152,29 +124,75 @@ export const AccountProfileDetails = (props) => {
                 variant="outlined"
               >
                 {states.map((option) => (
-                  <option
-                    key={option.value}
-                    value={option.value}
-                  >
+                  <option key={option.value} value={option.value}>
                     {option.label}
                   </option>
                 ))}
               </TextField>
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <TextField
+                fullWidth
+                label="Tennis Court Name"
+                name="tennisCourtName"
+                onChange={handleChange}
+                required
+                value={values.tennisCourtName}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <TextField
+                fullWidth
+                label="Tennis Court Description"
+                name="tennisCourtDesc"
+                onChange={handleChange}
+                required
+                value={values.tennisCourtDesc}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <TextField
+                fullWidth
+                label="Tennis Court Tag Line"
+                name="tennisCourtTag"
+                onChange={handleChange}
+                required
+                value={values.tennisCourtTag}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <Button
+                color="primary"
+                fullWidth
+                variant="outlined"
+                onClick={() => logoInput.current.click()}
+                sx={{ py: 1.6 }}
+              >
+                Upload Tennis Court Logo
+              </Button>
+              <input
+                style={{ display: "none" }}
+                type="file"
+                ref={logoInput}
+                value={values.tennisCourtLogo}
+                name="tennisCourtLogo"
+                onChange={handleChange}
+              />
             </Grid>
           </Grid>
         </CardContent>
         <Divider />
         <Box
           sx={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            p: 2
+            display: "flex",
+            justifyContent: "flex-end",
+            p: 2,
           }}
         >
-          <Button
-            color="primary"
-            variant="contained"
-          >
+          <Button color="primary" variant="contained">
             Save details
           </Button>
         </Box>
--- client2/src/components/account/account-profile.js
+++ client2/src/components/account/account-profile.js
@@ -1,3 +1,4 @@
+import { useRef, useState } from "react";
 import {
   Avatar,
   Box,
@@ -6,66 +7,89 @@ import {
   CardActions,
   CardContent,
   Divider,
-  Typography
-} from '@mui/material';
+  Typography,
+} from "@mui/material";
 
 const user = {
-  avatar: '/static/images/avatars/avatar_6.png',
-  city: 'Los Angeles',
-  country: 'USA',
-  jobTitle: 'Senior Developer',
-  name: 'Katarina Smith',
-  timezone: 'GTM-7'
+  avatar: "/static/images/avatars/avatar_6.png",
+  city: "Los Angeles",
+  country: "USA",
+  jobTitle: "Senior Developer",
+  name: "Katarina Smith",
+  timezone: "GTM-7",
 };
 
-export const AccountProfile = (props) => (
-  <Card {...props}>
-    <CardContent>
-      <Box
-        sx={{
-          alignItems: 'center',
-          display: 'flex',
-          flexDirection: 'column'
-        }}
-      >
-        <Avatar
-          src={user.avatar}
+export const AccountProfile = (props) => {
+  const [profilePic, setProfilePic] = useState("");
+  const [bannerPic, setBannerPic] = useState("");
+  const profileInput = useRef();
+  const bannerInput = useRef();
+
+  console.log("profile: ", profilePic);
+  console.log("banner: ", bannerPic);
+
+  return (
+    <Card {...props}>
+      <CardContent>
+        <Box
           sx={{
-            height: 64,
-            mb: 2,
-            width: 64
+            alignItems: "center",
+            display: "flex",
+            flexDirection: "column",
           }}
+        >
+          <Avatar
+            src={user.avatar}
+            sx={{
+              height: 64,
+              mb: 2,
+              width: 64,
+            }}
+          />
+          <Typography color="textPrimary" gutterBottom variant="h5">
+            {user.name}
+          </Typography>
+          <Typography color="textSecondary" variant="body2">
+            {`${user.city} ${user.country}`}
+          </Typography>
+          <Typography color="textSecondary" variant="body2">
+            {user.timezone}
+          </Typography>
+        </Box>
+      </CardContent>
+      <Divider />
+      <CardActions sx={{ display: "flex", flexDirection: "column" }}>
+        <Button
+          color="primary"
+          fullWidth
+          variant="text"
+          onClick={() => profileInput.current.click()}
+        >
+          Upload profile picture
+        </Button>
+        <input
+          style={{ display: "none" }}
+          type="file"
+          ref={profileInput}
+          value={profilePic}
+          onChange={(e) => setProfilePic(e.target.value)}
         />
-        <Typography
-          color="textPrimary"
-          gutterBottom
-          variant="h5"
+        <Button
+          color="primary"
+          fullWidth
+          variant="text"
+          onClick={() => bannerInput.current.click()}
         >
-          {user.name}
-        </Typography>
-        <Typography
-          color="textSecondary"
-          variant="body2"
-        >
-          {`${user.city} ${user.country}`}
-        </Typography>
-        <Typography
-          color="textSecondary"
-          variant="body2"
-        >
-          {user.timezone}
-        </Typography>
-      </Box>
-    </CardContent>
-    <Divider />
-    <CardActions>
-      <Button
-        color="primary"
-        fullWidth
-        variant="text"
-      >
-        Upload picture
-      </Button>
-    </CardActions>
-  </Card>
-);
+          Upload banner picture
+        </Button>
+        <input
+          style={{ display: "none" }}
+          type="file"
+          ref={bannerInput}
+          value={bannerPic}
+          onChange={(e) => setBannerPic(e.target.value)}
+        />
+      </CardActions>
+    </Card>
+  );
+};
