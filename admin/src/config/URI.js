cd1cf9a9b3a8a947f3057e3ecc16134be1de6b13
Shivanshudeveloper <developer.shivanshu@gmail.com>
2022-03-01 14:06:55 +0530

    W

--- admin/src/config/URI.js
+++ admin/src/config/URI.js
@@ -1,4 +1,5 @@
 // Local host
-const API_SERVICE = `http://localhost:5000`
+// const API_SERVICE = `http://localhost:5000`
+const API_SERVICE = `https://tennishapi.herokuapp.com`
 
 export { API_SERVICE }
